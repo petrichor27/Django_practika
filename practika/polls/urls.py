6153c8f10570abefec3201286531045f31fe398f
petrichor27 <elisabetbabenko@gmail.com>
2022-07-15 03:07:08 +0300

    1

--- practika/polls/urls.py
+++ practika/polls/urls.py
@@ -7,5 +7,8 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:rule_id>/', views.detail, name='detail'),
-    path('delete/', views.delete)
+    path('delete/', views.delete),
+    path('add/', views.add),
+    path('update/<int:rule_id>/', views.update, name='list_upd'),
+    path('update/<int:rule_id>/save/', views.save_update)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- practika/polls/views.py
+++ practika/polls/views.py
@@ -9,47 +9,42 @@ def index(request):
 def detail(request, rule_id):
     try:
         a = Rule.objects.get(id = rule_id)
+        t = Task.objects.filter(rule = a)
+        for i in t:
+            print(i.value)
+        # t1 = Task.objects.get.all()
+        
     except:
         raise Http404("Правило не найдено")
+    return render(request, 'polls/detail.html', {'rule': a.name, 'task': t, 'Dictionaries': Dictionaries})
 
-    return render(request, 'polls/detail.html', {'rule': a})
-
-#????
-# сохранение данных в бд
-def create(request):
+def add(request):
     if request.method == "POST":
         rule = Rule()
+        rule.name = request.POST.get("name_new")
+        rule.queue = request.POST.get("queue_new")
+        rule.rule_range = request.POST.get("rule_range_new")
+        rule.creator = 'admin'
+        rule.updator = 'admin'
+        rule.save()
+    return HttpResponseRedirect("/polls/")
+
+def update(request, rule_id):
+    rule_list = Rule.objects.order_by('name')
+    return render(request,'polls/list_upd.html', {'rule_list': rule_list, 'Dictionaries': Dictionaries, 'id_to_update': rule_id})
+    
+def save_update(request, rule_id):  
+    rule = Rule.objects.get(id = rule_id)
+    if request.method == "POST":
         rule.name = request.POST.get("name")
         rule.queue = request.POST.get("queue")
         rule.rule_range = request.POST.get("rule_range")
         rule.creator = 'admin'
         rule.updator = 'admin'
-        # rule.create_date = request.POST.get("create_date")
-        # rule.update_date = request.POST.get("update_date")
         rule.save()
-    return HttpResponseRedirect("polls/list.html")
+    rule_list = Rule.objects.order_by('name')
+    return HttpResponseRedirect("/polls/")
 
-# изменение данных в бд
-def edit(request, id):
-    try:
-        rule_list = Rule.objects.get(id = rule_id)
- 
-        if request.method == "POST":
-            rule_list.name = request.POST.get("name")
-            rule_list.queue = request.POST.get("queue")
-            rule_list.rule_range = request.POST.get("rule_range")
-            rule_list.creator = request.POST.get("creator")
-            rule_list.updator = request.POST.get("updator")
-            rule_list.create_date = request.POST.get("create_date")
-            rule_list.update_date = request.POST.get("update_date")
-            rule_list.save()
-            return HttpResponseRedirect("/polls/")
-        else:
-            return render(request, "list.html", {"rule_list": rule_list})
-    except Person.DoesNotExist:
-        return Http404("Rule not found")
-     
-# удаление данных из бд
 def delete(request):
     try:
         rule_del  = request.POST.getlist('checks')
